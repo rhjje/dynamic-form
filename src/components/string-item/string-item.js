c5d2f5e243a4ed3614406540f5bc5880ff741f88
Admin <numelen@gmail.com>
2021-08-08 00:26:51 +0300

    refactor: refactoring string item component

--- src/components/string-item/string-item.js
+++ src/components/string-item/string-item.js
@@ -6,16 +6,13 @@ const StringItem = ({ description, register, formName, errors,
   rules = { required: false, sizes: { min: 1, max: Infinity } }
 }) => {
   const { required, sizes: { min, max } } = rules;
-  let warning = '';
-  if (get(errors, formName)) {
-    warning = ' warning';
-  }
+
   return (
     <div className="mb-3">
       <label htmlFor={description} className="form-label">{description}</label>
       <input
         type="text"
-        className={`form-control${warning}`}
+        className={get(errors, formName) ? 'form-control warning' : 'form-control'}
         id={description}
         {...register(formName,
           {
